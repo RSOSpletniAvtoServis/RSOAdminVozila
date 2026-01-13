0909d3847b839c6fbea74bec92e012b502a47d41
mangart <zan.gostic1@gmail.com>
2026-01-13 13:46:51 +0100

    dodana poizvedba za kraje

--- main.py
+++ main.py
@@ -19,6 +19,35 @@ def read_items():
 def return_key():
     return {"Public key": "Kljuc"}
 
+@app.get("/kraji/")
+def get_kraji():
+    conn = mysql.connector.connect(
+        host="34.44.150.229",
+        user="zan",
+        password=">tnitm&+NqgoA=q6",
+        database="RSOAdminVozila"
+    )
+
+    # Create a cursor
+    cursor = conn.cursor()
+
+    # Run a SELECT query
+    query = "SELECT * FROM Kraj"
+    cursor.execute(query)
+
+    # Fetch all rows
+    rows = cursor.fetchall()
+
+    # Process results
+    for row in rows:
+        print(row)  # row is a tuple: (id, name, email)
+
+    # Clean up
+    cursor.close()
+    conn.close()   
+    return {"Kraji": "kraj"}
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
