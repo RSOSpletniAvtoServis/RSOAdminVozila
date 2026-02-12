e8fd8175e3136287aea0f028307672f349c88a14
mangart <zan.gostic1@gmail.com>
2026-02-12 13:26:13 +0100

    added restart policy

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def validate_identifier(name: str) -> str:
         raise ValueError("Invalid database name")
     return name
 
-
+db_healthy = True
 app = FastAPI(root_path="/admvoz")
 try:
     pool = mysql.connector.pooling.MySQLConnectionPool(
@@ -35,6 +35,7 @@ try:
     )
 except Exception as e:
     print("Error: ",e)
+    db_healthy = False
     
 app.add_middleware(
     CORSMiddleware,
@@ -1209,7 +1210,10 @@ def health():
     
 @app.get("/health/live")
 def live():
-    return {"status": "alive"}
+    if db_healthy:
+        return {"status": "alive"}
+    else:
+        return Response(status_code=500)
 
 @app.get("/health/ready")
 def ready():
